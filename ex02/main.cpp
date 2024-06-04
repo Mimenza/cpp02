ea9d1261c0b66c90da8c8ca6675b94b107c77298
Mimenza <mimenzae@gmail.com>
2024-06-04 17:32:03 +0200

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 16:20:17 by emimenza          #+#    #+#             */
+/*   Updated: 2024/06/04 16:23:01 by emimenza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "incs/Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return 0;
+}
